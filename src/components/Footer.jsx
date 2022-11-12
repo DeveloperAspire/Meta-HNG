e8aa726901dd6794927f605d9e2890b05c6287b0
VoN <hi@imrvon.com>
2022-11-13 00:45:12 +0100

    Header Mobile SVG

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom'
 function Footer() {
   return (
     <footer className=''>
-        <div className="footer-wrapper flex justify-between gap-5 px-24 py-12 bg-[#1D1D1E]">
-            <div className='flex flex-col gap-12'>
+        <div className="footer-wrapper flex flex-col sm:flex-wrap md:flex-nowrap sm:flex-row sm:gap-10 justify-between px-12 sm:px-24 py-12 bg-[#1D1D1E]">
+            <div className='flex flex-col gap-12 sm:w-[40%] md:w-auto'>
                 <div className='mb-12'><img src="img/logo-white.svg" alt="" /></div>
                 <div className='flex gap-12'>
                     <img src="img/facebook.svg" alt="Facebook" />
@@ -14,21 +14,21 @@ function Footer() {
                 </div>
                 <p className='text-left text-[#F7F7F7]'>© 2022 Metabnb</p>
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 sm:w-[40%] md:w-auto'>
                 <h3 className='text-white text-[18px] font-bold mb-2'>Community</h3>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>NFT</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Tokens</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Landlords</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Discord</NavLink>
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 sm:w-[40%] md:w-auto '>
                 <h3 className='text-white text-[18px] font-bold mb-2'>Places</h3>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Castle</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Farms</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Beach</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Learn more</NavLink>
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 sm:w-[40%] md:w-auto'>
                 <h3 className='text-white text-[18px] font-bold mb-2'>About us</h3>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Road map</NavLink>
                 <NavLink to={''} className='text-[#F1F3F9] text-sm'>Creators</NavLink>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,27 @@ import Modal from './Modal';
 function Header() {
   const [openModal, setOpenModal] = useState(false);
 
+  const [mobileMenu, setOpenMenu] = useState(false);
+
   return (
     <>
-      <header className='flex justify-between items-center px-24 py-12'>
-          <div><img src="img/logo.svg" alt="" /></div>
-          <nav className='flex gap-12'>
-              <NavLink to={'/'}  id='locations'>Home</NavLink>
-              <NavLink to={'/locations'}  id='locations'>Place to stay</NavLink>
-              <NavLink to={''}>NFTs</NavLink>
-              <NavLink to={''}>Community</NavLink>
+      <header className='flex justify-between items-center px-12 sm:px-24 py-12 flex-wrap'>
+          <div className='w-[70%] min-[896px]:w-[20%]'><img src="img/logo.svg" alt="" className='w-full'/></div>
+          <nav className='w-[20%] min-[896px]:w-[70%] flex max-[896px]:justify-end min-[896px]:justify-between'>
+              <div className='flex items-center justify-between gap-4 max-[895px]:hidden w-full' id="menu">
+                <NavLink to={'/'}  id='locations'>Home</NavLink>
+                <NavLink to={'/locations'}  id='locations'>Place to stay</NavLink>
+                <NavLink to={''}>NFTs</NavLink>
+                <NavLink to={''}>Community</NavLink>
+                <button className='rounded text-white header-button py-2 px-4 whitespace-nowrap' id='connect-wallet'
+                          onClick={() => {
+                            setOpenModal(true)}}>Connect wallet</button>
+              </div>
+              <svg className="w-6 z-20 relative min-[896px]:hidden" id="toggle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
           </nav>
-          <button className='rounded text-white header-button py-2 px-4' id='connect-wallet'
-          onClick={() => {
-            setOpenModal(true)}}>Connect wallet</button>
+          
       </header>      
       {openModal && <Modal closeUpModal={setOpenModal} />}
     </>
